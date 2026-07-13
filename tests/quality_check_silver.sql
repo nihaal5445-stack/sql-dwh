1395d7c4a8f1581d96dc9eefb679b77e97794eda
nihaal5445-stack <nihaal5445@gmail.com>
2026-07-13 13:36:19 +0530

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -1,10 +1,29 @@
-set search_path to bronze
---=========================
---					crm_cust_info
---=========================
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy,
+    and standardization across the 'silver' schema. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
 
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
 
--- check for nulls and duplicates
+-- ===============================================================================
+-- Checking 'silver.crm_cust_info'
+-- ===============================================================================
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+
 select cst_id , count(*) from silver.crm_cust_info
 group by cst_id having count(*)>1
 
